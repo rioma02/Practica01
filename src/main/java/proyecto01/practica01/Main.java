e93b9b4d5373970ee203858aa62b59aad00f0289
AdryHD <152119270+AdryHD@users.noreply.github.com>
2025-02-09 22:46:34 -0600

    Prueba 3
    Prueba 3

--- src/main/java/proyecto01/practica01/Main.java
+++ src/main/java/proyecto01/practica01/Main.java
@@ -16,5 +16,6 @@ public class Main {
         System.out.println(resultadoEgreso);
         System.out.println("Prueba Adry");
         System.out.println("Prueba Adry2");
+        System.out.println("Prueba Adry3");
     }
 }
